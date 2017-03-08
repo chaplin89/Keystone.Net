1d00aaffc75d40fed1dc3afb42c629fe39f33004
Marco <Marco>
2017-03-08 10:05:53 +0100

    Removed useless unsafe; Made function that does not require a pointer to "engine" static.

--- Keystone.Net/Keystone.cs
+++ Keystone.Net/Keystone.cs
@@ -1,19 +1,40 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace Keystone
 {
+    /// <summary>
+    /// Manage a Keystone engine.
+    /// </summary>
     public class Keystone : IDisposable
     {
         IntPtr engine = IntPtr.Zero;
 
-        public Keystone(KeystoneArchitecture architecture, KeystoneMode mode)
+        /// <summary>
+        /// Construct the object with a given architecture and a given mode.
+        /// </summary>
+        /// <param name="architecture">Architecture</param>
+        /// <param name="mode">Mode, i.e. endianess, word size etc.</param>
+        /// <remarks>
+        /// Some architectures are not supported.
+        /// Check with <see cref="IsArchitectureSupported(KeystoneArchitecture)"/> if the engine
+        /// support the architecture.
+        /// </remarks>
+        public Keystone(KeystoneArchitecture architecture, KeystoneMode mode, bool throwIfError = true)
         {
             var result = KeystoneImports.Open(architecture, (int)mode, ref engine);
             if (result != KeystoneError.KS_ERR_OK)
+
+
                 throw new InvalidOperationException($"Error while initializing keystone: {ErrorToString(result)}");
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="value"></param>
         public void SetOption(OptionType type, uint value)
         {
             var result = KeystoneImports.SetOption(engine, type, value);
@@ -21,17 +42,25 @@ namespace Keystone
                 throw new InvalidOperationException($"Error while setting option in keystone: {ErrorToString(result)}");
         }
 
-        private string ErrorToString(KeystoneError result)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public static string ErrorToString(KeystoneError result)
         {
-            unsafe
-            {
-                IntPtr error = (IntPtr)KeystoneImports.ErrorToString(result);
-                if (error != IntPtr.Zero)
-                    return Marshal.PtrToStringAnsi(error);
-                return string.Empty;
-            }
+            IntPtr error = KeystoneImports.ErrorToString(result);
+            if (error != IntPtr.Zero)
+                return Marshal.PtrToStringAnsi(error);
+            return string.Empty;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="toEncode"></param>
+        /// <param name="address"></param>
+        /// <returns></returns>
         public KeystoneEncoded Assemble(string toEncode, ulong address)
         {
             IntPtr encoding;
@@ -39,12 +68,12 @@ namespace Keystone
             uint statementCount;
             byte[] buffer;
 
-            int result = KeystoneImports.Assemble(engine,
-                toEncode,
-                address,
-                out encoding,
-                out size,
-                out statementCount);
+            int result = KeystoneImports.Assemble(engine, 
+                                                  toEncode, 
+                                                  address, 
+                                                  out encoding, 
+                                                  out size, 
+                                                  out statementCount);
 
             if (result != 0)
                 throw new InvalidOperationException($"Error while assembling {toEncode}: {ErrorToString(GetLastKeystoneError())}");
@@ -52,35 +81,58 @@ namespace Keystone
             buffer = new byte[size];
 
             unsafe
-            { 
+            {
                 byte* ptrBuffer = (byte*)encoding;
                 Marshal.Copy(encoding, buffer, 0, (int)size);
-                KeystoneImports.Free(encoding);
             }
 
+            KeystoneImports.Free(encoding);
             return new KeystoneEncoded(buffer, statementCount);
         }
 
+        /// <summary>
+        /// Get the last error for this instance.
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// 
+        /// </remarks>
         public KeystoneError GetLastKeystoneError()
         {
             return KeystoneImports.GetLastKeystoneError(engine);
         }
 
-        public bool IsArchitectureSupported(KeystoneArchitecture architecture)
+        /// <summary>
+        /// Check if an architecture is supported.
+        /// </summary>
+        /// <param name="architecture">Architecture</param>
+        /// <returns>True if it is supported</returns>
+        public static bool IsArchitectureSupported(KeystoneArchitecture architecture)
         {
             return KeystoneImports.IsArchitectureSupported(architecture);
         }
 
-        public uint GetKeystoneVersion(ref uint major, ref uint minor)
+        /// <summary>
+        /// Get the version of the engine.
+        /// </summary>
+        /// <param name="major"></param>
+        /// <param name="minor"></param>
+        /// <returns></returns>
+        public static uint GetKeystoneVersion(ref uint major, ref uint minor)
         {
             return KeystoneImports.Version(ref major, ref minor);
         }
-
+        
+        /// <summary>
+        /// Release the engine.
+        /// </summary>
         public void Dispose()
         {
-            if (engine != IntPtr.Zero)
-                KeystoneImports.Close(engine);
-            engine = IntPtr.Zero;
+            var currentEngine = Interlocked.Exchange(ref engine, IntPtr.Zero);
+            if (currentEngine != IntPtr.Zero)
+                KeystoneImports.Close(currentEngine);
+
+            GC.SuppressFinalize(this);
         }
     }
 }
--- Keystone.Net/KeystoneImports.cs
+++ Keystone.Net/KeystoneImports.cs
@@ -1,40 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Keystone
 {
-    public class KeystoneImports
+    /// <summary>
+    /// Imports for keystone.dll
+    /// </summary>
+    internal class KeystoneImports
     {
         [DllImport("keystone.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ks_version" )]
-        public extern static uint Version(ref uint major, ref uint minor);
+        internal extern static uint Version(ref uint major, ref uint minor);
         
         [DllImport("keystone.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ks_open")]
-        public extern unsafe static KeystoneError Open(KeystoneArchitecture arch, int mode, ref IntPtr ks);
+        internal extern static KeystoneError Open(KeystoneArchitecture arch, int mode, ref IntPtr ks);
 
         [DllImport("keystone.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ks_close")]
-        public extern unsafe static KeystoneError Close(IntPtr ks);
+        internal extern static KeystoneError Close(IntPtr ks);
 
         [DllImport("keystone.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ks_free")]
-        public extern unsafe static void Free(IntPtr buffer);
+        internal extern static void Free(IntPtr buffer);
 
         [DllImport("keystone.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ks_strerror")]
-        public extern static unsafe char* ErrorToString(KeystoneError code);
+        internal extern static IntPtr ErrorToString(KeystoneError code);
         
         [DllImport("keystone.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ks_errno")]
-        public extern static KeystoneError GetLastKeystoneError(IntPtr ks);
+        internal extern static KeystoneError GetLastKeystoneError(IntPtr ks);
         
         [DllImport("keystone.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ks_arch_supported")]
-        public extern static bool IsArchitectureSupported(KeystoneArchitecture arch);
+        internal extern static bool IsArchitectureSupported(KeystoneArchitecture arch);
 
         [DllImport("keystone.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ks_option")]
-        public extern static KeystoneError SetOption(IntPtr ks, OptionType type, uint value);
+        internal extern static KeystoneError SetOption(IntPtr ks, OptionType type, uint value);
 
         [DllImport("keystone.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "ks_asm")]
-        public extern unsafe static int Assemble(IntPtr ks, 
+        internal extern static int Assemble(IntPtr ks, 
             [MarshalAs(UnmanagedType.LPStr)] string toEncode, 
             ulong baseAddress, 
             out IntPtr encoding,
