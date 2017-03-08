cecd5abbfd7bf9a437c6bb13f6acfd87347e853c
Marco <Marco>
2017-03-08 17:04:38 +0100

    Added feature file for compilation and relative steps

--- Keystone.Net.Tests/CompilationSteps.cs
+++ Keystone.Net.Tests/CompilationSteps.cs
@@ -11,33 +11,31 @@ namespace KeystoneBindingsTests
     class CompilationSteps
     {
         [Given(@"An instance of Keystone built for (.*) in mode (.*)")]
-        public void GivenAnInstanceOfKeystoneBuiltForXInMode(string p0, string p1)
+        public void GivenAnInstanceOfKeystoneBuiltForInMode(string p0, string p1)
         {
-            ScenarioContext.Current.Pending();
         }
 
         [Given(@"The statements ""(.*)""")]
         public void GivenTheStatements(string p0)
         {
-            ScenarioContext.Current.Pending();
         }
 
         [When(@"I compile the statement with Keystone")]
         public void WhenICompileTheStatementWithKeystone()
         {
-            ScenarioContext.Current.Pending();
+            
         }
 
         [Then(@"the result is (.*)")]
         public void ThenTheResultIs(string p0)
         {
-            ScenarioContext.Current.Pending();
+            
         }
 
         [Then(@"The last error is (.*)")]
-        public void ThenTheLastErrorIsEXPR_TOKEN()
+        public void ThenTheLastErrorIs()
         {
-            ScenarioContext.Current.Pending();
+            
         }
 
     }
